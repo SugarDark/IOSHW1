5ca60c1b7ad6d828f0c9ab7da72213b10a5ae171
SugarDark <joey861121@gmail.com>
2019-03-23 14:59:34 +0800

    Add files via upload

---
+++ hw1/hw1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hw1
+//
+//  Created by 方九毅 on 2019/3/23.
+//  Copyright © 2019 方九毅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
